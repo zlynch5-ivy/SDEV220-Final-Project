61c6b2721b2b17cc4a9a36859d4969fb1cf5794a
zlynch5-ivy <zlynch5@ivytech.edu>
2026-07-02 20:05:16 -0400

    Add files via upload
    Basic main tab for the project.

---
+++ SDEV Final Project.py
@@ -0,0 +1,20 @@
+# Final Project
+# Created on: 6/23/2026
+# By Zach Lynch, Ean Miller, Ebony Harris, Jayden Judge
+
+menu_tabs = ["Starters", "Burgers", "Steaks", "Chicken", "Sides", "Drinks"]
+
+menu_Starters = ["Cactus Blossom", "Cheese Fries", "Texas Red Chili", "Combo Appetizer", "Rattlesnake Bites", "Boneless Buffalo Wings"]
+
+menu_Burgers = ["All-American Cheeseburger", "Bacon Cheeseburger", "Smokehouse Burger", "Pulled Pork Sandwich", "BBQ Chicken Sandwich", "Mushroom Jack Chicken Sandwich"]
+# Toppings below is unfinished, everything else is the basic idea for the menu. - Zach
+menu_Burgers_toppings = ["Add Ketchup", "Add Mustard", "Add Pickles", "Remove Tomato", "Remove Onion", "Remove Lettuce"]
+
+menu_Steaks = ["Hand-Cut Sirloin", "Bone-in Ribeye", "Road Kill", "Steak Kabob", "Prime Rib", "Porterhouse T-Bone"]
+menu_Steaks_cook = ["Rare", "Medium-Rare", "Medium", "Medium-Well", "Well"]
+
+menu_Chicken = ["Country-Fried Chicken", "Chicken Critters", "Smothered Chicken", "Herb-Crusted Chicken", "Grilled BBQ Chicken", "Portobello Mushroom Chicken"]
+
+menu_Sides = ["Sauteed Mushrooms", "Mac & Cheese", "House Salad", "Texas Red Chili", "Steamed Vegetables", "Sidekick of Ribs"]
+
+menu_Drinks = ["20 Oz. Fountain Drink", "20 Oz. Lemonade", "20 Oz. Iced Tea", "20 Oz. Sweet Tea", "Bottled Water", "Mix & Match Tea or Lemonade"]
